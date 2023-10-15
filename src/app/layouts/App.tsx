d51760ff86d700dfe7a13d3db54545ae76e3f576
Daniel Luke <daniel@ringandrod.com>
2023-10-15 09:23:17 -0700

    End of Section 3

--- src/app/layouts/App.tsx
+++ src/app/layouts/App.tsx
@@ -1,5 +1,18 @@
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/dashboard/EventDashboard";
+import NavBar from "./nav/NavBar";
+import { useState } from "react";
+
 function App() {
-  return <h1>Welcome to Revents</h1>;
+  const [formOpen, setFormOpen] = useState(false);
+  return (
+    <>
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className="main">
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </>
+  );
 }
 
 export default App;
